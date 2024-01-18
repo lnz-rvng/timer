26b31104799a7e959de1618db96dcafe11bdc1c7
Lance <lanceirvingmanzanal@icloud.com>
2024-01-18 01:23:09 -0700

    implement an alarm/timer function

---
+++ timer1.js
@@ -0,0 +1,28 @@
+// process.stdout.write('\x07');
+
+const alarm = (seconds) => {
+  setTimeout(() => {
+    process.stdout.write('\x07 Beep \n');
+  }, seconds * 1000);
+};
+
+const argv = function() {
+  const args = process.argv.slice(2);
+
+  if (args.length === 0) {
+    console.log(`No input. Please enter a positive integer`);
+  }
+  
+  args.forEach((arg) => {
+    const seconds = parseInt(arg, 10);
+    if (isNaN(seconds)) {
+      console.log(`Invalid input. Enter integer only`);
+    }
+    if (seconds < 0) {
+      console.log(`Invalid input. Enter positive integer only`);
+    }
+    alarm(seconds);
+  });
+};
+
+argv();
